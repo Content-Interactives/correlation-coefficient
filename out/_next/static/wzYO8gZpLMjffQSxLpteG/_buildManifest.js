bf9593428a78eaf4351db927976a2ca04ad8bc16
Adrian Salmon <adriansalmon@adrians-mbp.lan>
2025-03-14 11:29:59 -0700

    Update build

--- out/_next/static/wzYO8gZpLMjffQSxLpteG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/106-bceb1c2a0c35b50c.js","static/chunks/pages/index-7e7fe4dcb563a037.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
