3bd9a6157d0b017a2590ebcb8e5b66a1618d8016
Adrian Salmon <adriansalmon@adrians-mbp.lan>
2025-03-14 14:41:48 -0700

    Remove gray background while maintaining interactive size

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import CorrelationExplorer from '../components/CorrelationExplorer'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+    <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <CorrelationExplorer />
       </div>
